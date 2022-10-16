1181e2f894410e2ecc04d0f07bdde0fad83e7a42
hydze <hhyde@protonmail.com>
2022-10-16 02:04:24 -0400

    MENU REVAMP 10.15.22
    -Created and scripted main menu
    -Created and scripted leaderboards menu
    
    K.E
    -player sticks to border

---
+++ CIS267_EndlessRunner_JessePerry/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void leaderboardMenu()
+    {
+        SceneManager.LoadScene("LeaderboardsScene");
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
